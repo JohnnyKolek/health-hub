d3188d5b7634a447343b30d01e72da67ad223f14
Jan Kołek <jankolek0@gmail.com>
2024-04-05 20:48:08 +0200

    defining dependencies and Main class

---
+++ src/main/java/kolek/jan/healthhub/HealthHubBackendApplication.java
@@ -0,0 +1,12 @@
+package kolek.jan.healthhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHubBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HealthHubBackendApplication.class, args);
+
+    }
+}
