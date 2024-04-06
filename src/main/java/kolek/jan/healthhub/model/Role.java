a9ec96eaa9a5b2594c3447dd03b77fe9ae2b416c
Jan Kołek <jankolek0@gmail.com>
2024-04-06 18:12:58 +0200

    create entities and user repository

---
+++ src/main/java/kolek/jan/healthhub/model/Role.java
@@ -0,0 +1,23 @@
+package kolek.jan.healthhub.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+}
