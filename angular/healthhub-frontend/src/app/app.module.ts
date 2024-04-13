8d4bf57d75a062a5772e01fa2a32b4c84095a34e
Jan Kołek <jankolek0@gmail.com>
2024-04-14 00:43:12 +0200

    FE: add Doctors and Doctor components

--- angular/healthhub-frontend/src/app/app.module.ts
+++ angular/healthhub-frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DoctorsComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
