5e5e8b927c254f0f7d8caa16ca24a14458e0ac94
Jan Kołek <jankolek0@gmail.com>
2024-04-14 15:46:06 +0200

    FE: enable routing

--- angular/healthhub-frontend/src/app/app.module.ts
+++ angular/healthhub-frontend/src/app/app.module.ts
@@ -5,18 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DoctorsComponent } from './doctors/doctors.component';
-import { DoctorComponent } from './doctor/doctor.component';
+import { DoctorComponent } from './doctors/doctor/doctor.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { PersonalDataComponent } from './personal-data/personal-data.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'doctors', component: DoctorsComponent},
+  {path: 'personalData', component: PersonalDataComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DoctorsComponent,
-    DoctorComponent
+    DoctorComponent,
+    HomeComponent,
+    PersonalDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
